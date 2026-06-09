1282e6be31bb7d46d351bd12cfecdef834093eed
derrick <drocha616@gmail.com>
2026-06-09 17:15:03 -0600

    Added internet permission to android.

--- shared/src/commonMain/kotlin/org/example/asw_portal_kmp/network/api/NetworkManager.kt
+++ shared/src/commonMain/kotlin/org/example/asw_portal_kmp/network/api/NetworkManager.kt
@@ -1,8 +1,22 @@
 package org.example.asw_portal_kmp.network.api
 
-class NetworkManager {
+import io.ktor.client.HttpClient
+import io.ktor.client.request.request
+import io.ktor.http.HttpMethod
+import io.ktor.http.headers
 
-    suspend fun get() {
+data class RequestOptions(val isAuthRequired: Boolean = false, val isTenantRequired: Boolean = false, val timeout: Long? = 10000)
 
+class NetworkManager(val client: HttpClient) {
+
+    private fun getHeaders() = headers {
+
+    }
+
+    suspend fun get(url: String, params: Map<String, String>, options: RequestOptions) {
+        val response = client.request(url) {
+            method = HttpMethod.Get
+            getHeaders()
+        }
     }
 }
