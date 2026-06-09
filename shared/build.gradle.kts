e7ab5e0253faca9bb36b315c9813ff150519d624
derrick <drocha616@gmail.com>
2026-06-09 13:13:21 -0600

    Added ktor client dependency.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -49,6 +49,8 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.cio)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
