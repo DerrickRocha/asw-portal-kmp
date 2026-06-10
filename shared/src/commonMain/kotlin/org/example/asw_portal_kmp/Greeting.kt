a9e7e102e0467c9da0384306cf570d5808b13b51
derrick <drocha616@gmail.com>
2026-06-09 20:44:32 -0600

    Added get method minus auth and tenant id.

--- shared/src/commonMain/kotlin/org/example/asw_portal_kmp/Greeting.kt
+++ shared/src/commonMain/kotlin/org/example/asw_portal_kmp/Greeting.kt
@@ -1,9 +1,30 @@
 package org.example.asw_portal_kmp
 
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import org.example.asw_portal_kmp.network.api.NetworkManager
+import org.example.asw_portal_kmp.network.api.RequestOptions
+
 class Greeting {
     private val platform = getPlatform()
+    private val client = HttpClient(){
+        defaultRequest {
+            url("http://10.0.2.2:5100")
+            contentType(ContentType.Application.Json)
+        }
+        expectSuccess = true
+    }
+    private val networkManager = NetworkManager(client)
+    suspend fun greet(): String {
+        try {
+            val response = networkManager.get("/auth/login", emptyMap(), RequestOptions())
+            println(response)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
 
-    fun greet(): String {
         return sayHello(platform.name)
     }
 }
--- shared/src/commonMain/kotlin/org/example/asw_portal_kmp/network/api/NetworkManager.kt
+++ shared/src/commonMain/kotlin/org/example/asw_portal_kmp/network/api/NetworkManager.kt
@@ -1,22 +1,46 @@
 package org.example.asw_portal_kmp.network.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.request
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
+import io.ktor.http.contentType
 import io.ktor.http.headers
 
 data class RequestOptions(val isAuthRequired: Boolean = false, val isTenantRequired: Boolean = false, val timeout: Long? = 10000)
 
 class NetworkManager(val client: HttpClient) {
 
-    private fun getHeaders() = headers {
+    private fun getHeaders(isAuthRequired: Boolean, isTenantRequired: Boolean) = headers {
+        if (isAuthRequired) {
+            // get token
+            // throw error if token not found
+            // add token to header
+        }
 
+        if (isTenantRequired) {
+            // get tenantId
+            // throw error if tenantId not found
+            // add tenantId to header (X-Tenant-Id).
+        }
     }
 
-    suspend fun get(url: String, params: Map<String, String>, options: RequestOptions) {
+    suspend fun get(url: String, params: Map<String, String>, options: RequestOptions): String {
         val response = client.request(url) {
             method = HttpMethod.Get
-            getHeaders()
+            contentType(ContentType.Application.Json)
+            headers {
+                getHeaders(options.isAuthRequired, options.isTenantRequired)
+            }
+            url {
+                params.forEach { (key, value) ->
+                    parameters.append(key, value)
+                }
+            }
         }
+        val body = response.bodyAsText()
+        return body
     }
 }
