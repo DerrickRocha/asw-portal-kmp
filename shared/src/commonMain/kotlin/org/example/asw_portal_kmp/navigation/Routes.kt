7cde1138e01b4b8aeb28337f63f42f9f139c1396
Derrick Rocha <drocha616@gmail.com>
2026-06-16 02:44:09 -0600

    Add-Navigation (#2)
    * Added routes and nav3 dependencies.
    
    * Removed comments.

---
+++ shared/src/commonMain/kotlin/org/example/asw_portal_kmp/navigation/Routes.kt
@@ -0,0 +1,50 @@
+package org.example.asw_portal_kmp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.savedstate.serialization.SavedStateConfiguration
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+
+@Serializable
+sealed interface Route: NavKey {
+
+    @Serializable
+    data object Login: Route
+
+    @Serializable
+    data object TenantSelection: Route
+    @Serializable
+    data class TenantConsole(val tenantId: Int): Route
+    @Serializable
+    data object Products: Route
+    @Serializable
+    data class ProductDetails(val productId: Int): Route
+    @Serializable
+    data object Orders: Route
+    @Serializable
+    data class OrderDetails(val orderId: Int): Route
+    @Serializable
+    data object Inventories: Route
+    @Serializable
+    data class InventoryItemDetails(val inventoryId: Int): Route
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+val navigationConfig: SavedStateConfiguration
+    get() = SavedStateConfiguration {
+        serializersModule = SerializersModule {
+            polymorphic(NavKey::class) {
+                subclassesOfSealed<Route>()
+            }
+        }
+    }
+
+@Composable
+fun rememberECommerceNavBackStack(initialRoute: Route): NavBackStack<NavKey> {
+    return rememberNavBackStack(navigationConfig, initialRoute)
+}
