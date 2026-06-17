a0027021006133f25d4af59058d2cad6205d3428
derrick <drocha616@gmail.com>
2026-06-17 14:36:03 -0600

    added appviewmodel tests.

--- shared/src/commonTest/kotlin/org/example/asw_portal_kmp/ui/viewModels/AppViewModelTest.kt
+++ shared/src/commonTest/kotlin/org/example/asw_portal_kmp/ui/viewModels/AppViewModelTest.kt
@@ -28,4 +28,30 @@ class AppViewModelTest {
         assertEquals(123, effect.tenantId)
         verifySuspend { mockKeyValueManager.getTenantId() }
     }
+
+    @Test
+    fun testInit_whenUserLoggedInAndTenantIdDoesNotExist_thenNavigateToTenantSelection() = runTest {
+        everySuspend { mockKeyValueManager.isLoggedIn } returns flowOf(true)
+        everySuspend { mockKeyValueManager.getTenantId() } returns null
+        val viewModel = AppViewModel(mockKeyValueManager)
+        val effect = viewModel.effects.first()
+        assertTrue(effect is AppEffects.NavigateToTenantSelection)
+    }
+
+    @Test
+    fun testInit_whenUserLoggedInAndTenantIdIsNegative_thenNavigateToTenantSelection() = runTest {
+        everySuspend { mockKeyValueManager.isLoggedIn } returns flowOf(true)
+        everySuspend { mockKeyValueManager.getTenantId() } returns -1
+        val viewModel = AppViewModel(mockKeyValueManager)
+        val effect = viewModel.effects.first()
+        assertTrue(effect is AppEffects.NavigateToTenantSelection)
+    }
+
+    @Test
+    fun testInit_whenUserNotLoggedIn_thenNavigateToLogin() = runTest {
+        everySuspend { mockKeyValueManager.isLoggedIn } returns flowOf(false)
+        val viewModel = AppViewModel(mockKeyValueManager)
+        val effect = viewModel.effects.first()
+        assertTrue(effect is AppEffects.NavigateToLogin)
+    }
 }
